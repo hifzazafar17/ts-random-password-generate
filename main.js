2b169b1e933cb4be3be169fa3ccebea0b16d516e
Hifza Zafar <152292315+Hifza-Code-Journey@users.noreply.github.com>
2024-03-20 09:59:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+const generatePassword = (length) => {
+    const charset = 'abcdefghijklmnopqrstuvwxyzQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()_+{}|:<>?~';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+};
+const mainMenuPrompt = async () => {
+    const { length } = await inquirer.prompt([
+        {
+            type: "number",
+            name: "length",
+            message: "Enter password length:",
+            default: 12
+        },
+    ]);
+    const password = generatePassword(length);
+    console.log(`Generate Password: ${password}`);
+};
+console.log("Welcome to the Password Generator CLI App\n");
+mainMenuPrompt();
